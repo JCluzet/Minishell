3648bd3013a29c4d3f1b439b390c36f5f3c467ca
Vigorate <aminebelka14@gmail.com>
2021-09-22 15:09:15 +0200

    Commit through vscode

--- srcs/execution/dispatcher.c
+++ srcs/execution/dispatcher.c
@@ -6,7 +6,7 @@
 /*   By: ambelkac <ambelkac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/16 19:11:06 by ambelkac          #+#    #+#             */
-/*   Updated: 2021/09/22 14:46:29 by ambelkac         ###   ########.fr       */
+/*   Updated: 2021/09/22 15:08:00 by ambelkac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,37 @@
 void (*builtins_array[7])(t_sdata *) = {display_env, shell_export, shell_unset, pwd,
 	echo, shell_cd, shell_exit};
 
+void	manage_fd_dups(t_sdata sdata)
+{
+	// Cascade Dup redirection inward
+
+	// Cascade Dup redicrection outward
+
+	// 
+}
+
 void		execution_dispatcher(t_sdata *sdata)
 {
 	pid_t	pid;
+	int		i;
 
-	if (!sdata->cmds)
-		return ;
-	if (sdata->cmds->builtin_idx < 7 && sdata->cmds->builtin_idx != -1)
-		(builtins_array)[sdata->cmds->builtin_idx](sdata);
-	else if (sdata->cmds->cmd_path)
+	i = 0;
+	while (i < sdata->cmd_nbr)
 	{
-		pid = fork();
-		if (!pid)
+		if (sdata->cmds->builtin_idx < 7 && sdata->cmds->builtin_idx != -1)
+			(builtins_array)[sdata->cmds->builtin_idx](sdata);
+		else if (sdata->cmds->cmd_path)
 		{
-			execve(sdata->cmds->cmd_path, sdata->cmds->argv, sdata->env);
-		}
-		else
-		{
-			waitpid(-1, NULL, 0);
+			manage_fd_dups();
+			pid = fork();
+			if (!pid)
+			{
+				execve(sdata->cmds->cmd_path, sdata->cmds->argv, sdata->env);
+			}
+			else
+			{
+				waitpid(-1, NULL, 0);
+			}
 		}
 	}
 }
