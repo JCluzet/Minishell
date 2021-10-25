5a72fca1f16f0b547dabc9f8d36dcd5666f1b3b5
Vigorate <aminebelka14@gmail.com>
2021-10-25 14:19:17 +0200

    pipe on builtins cmd

--- srcs/execution/dispatcher.c
+++ srcs/execution/dispatcher.c
@@ -6,7 +6,7 @@
 /*   By: ambelkac <ambelkac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/16 19:11:06 by ambelkac          #+#    #+#             */
-/*   Updated: 2021/10/22 17:33:17 by ambelkac         ###   ########.fr       */
+/*   Updated: 2021/10/25 14:18:37 by ambelkac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,18 @@
 void (*builtins_array[7])(t_sdata *) = {display_env, shell_export, shell_unset, pwd,
 	echo, shell_cd, shell_exit};
 
-void	manage_fd_dups(t_cmd_lst *cmds, pid_t pid, int *fd)
+void	manage_pipe_dups(t_cmd_lst *cmds, pid_t pid, int *fd)
 {
 	if (cmds->next)
-	{
 		if (!pid)
 		{
 			close(fd[0]);
 			dup2(fd[1], 1);
 		}
-		else
-		{
-			close(fd[1]);
-			dup2(fd[0], 0);
-		}
-	}
+}
+
+int		manage_builtin_dup(t_cmd_lst *cmds, int *fd)
+{
 }
 
 void		execution_dispatcher(t_sdata *sdata, t_cmd_lst *cmds)
@@ -38,25 +35,39 @@ void		execution_dispatcher(t_sdata *sdata, t_cmd_lst *cmds)
 	pid_t	pid;
 	int		i;
 	int		save_stdin;
+	int		save_stdout;
 
 	save_stdin = dup(0);
+	save_stdout = dup(1);
 	i = 0;
 	while (cmds)
 	{
+		if (cmds->next)
+			pipe(fd);
 		if (cmds->builtin_idx < 7 && cmds->builtin_idx != -1)
+		{	
+			if (cmds->next)
+				dup2(fd[1], 1);
 			(builtins_array)[cmds->builtin_idx](sdata);
+			dup2(save_stdout, 1);
+		}
 		else if (cmds->cmd_path)
 		{
-			pipe(fd);
 			pid = fork();
-			manage_fd_dups(cmds, pid, fd);
+			manage_pipe_dups(cmds, pid, fd);
 			if (!pid)
 				execve(cmds->cmd_path, cmds->argv, sdata->env);
 			else
 				waitpid(-1, NULL, 0);
 		}
+		if (cmds->next)
+		{
+			close(fd[1]);
+			dup2(fd[0], 0);
+		}
 		cmds = cmds->next;
 	}
 	dup2(save_stdin, 0);
 	close(save_stdin);
+	close(save_stdout);
 }
