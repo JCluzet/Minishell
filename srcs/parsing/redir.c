ff528b852ceb1aa3566e5580ba15dd37d6dc6325
Jo <jo@JosMac.home>
2021-11-13 16:15:50 +0100

    Fix redir norme error

--- srcs/parsing/redir.c
+++ srcs/parsing/redir.c
@@ -6,7 +6,7 @@
 /*   By: jo <jo@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/29 19:17:46 by jcluzet           #+#    #+#             */
-/*   Updated: 2021/11/13 15:50:21 by jo               ###   ########.fr       */
+/*   Updated: 2021/11/13 16:12:11 by jo               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,32 +22,16 @@ char	*fill_file_rdr(char *cmd, int nb, int type, char *file)
 	while (cmd[i])
 	{
 		i = find_quotes(cmd, i, cmd[i]);
-		if (cmd[i] == '<' && cmd[i + 1] == '<')
-		{
-			rdr.nb_redir_hdoc++;
-			if (type == 4 && rdr.nb_redir_hdoc == nb)
-				return (get_file_redir(cmd + i + 2, file));
-			i++;
-		}
-		else if (cmd[i] == '>' && cmd[i + 1] == '>')
-		{
-			rdr.nb_redir_app++;
-			if (type == 3 && rdr.nb_redir_app == nb)
-				return (get_file_redir(cmd + i + 2, file));
-			i++;
-		}
-		else if (cmd[i] == '<')
-		{
-			rdr.nro++;
-			if (type == 2 && rdr.nro == nb)
-				return (get_file_redir(cmd + i + 1, file));
-		}
-		else if (cmd[i] == '>')
-		{
-			rdr.nri++;
-			if (type == 1 && rdr.nri == nb)
-				return (get_file_redir(cmd + i + 1, file));
-		}
+		if ((cmd[i] == '<' && cmd[i + 1] == '<') && ++i > -1
+			&& ++rdr.nb_redir_hdoc == nb && type == 4)
+			return (get_file_redir(cmd + i + 2, file));
+		else if ((cmd[i] == '>' && cmd[i + 1] == '>') && ++i > -1
+			&& ++rdr.nb_redir_app == nb && type == 3)
+			return (get_file_redir(cmd + i + 2, file));
+		else if (cmd[i] == '<' && ++rdr.nro == nb && type == 2)
+			return (get_file_redir(cmd + i + 1, file));
+		else if (cmd[i] == '>' && ++rdr.nri == nb && type == 1)
+			return (get_file_redir(cmd + i + 1, file));
 		i++;
 	}
 	return (NULL);
@@ -63,79 +47,68 @@ int	find_size_rdr(char *cmd, int nb, int type)
 	while (cmd[i])
 	{
 		i = find_quotes(cmd, i, cmd[i]);
-		if (cmd[i] == '<' && cmd[i + 1] == '<')
-		{
-			rdr.nb_redir_hdoc++;
-			if (type == 4 && rdr.nb_redir_hdoc == nb)
-				return (find_lenght_file(cmd + i + 2));
-			i++;
-		}
-		else if (cmd[i] == '>' && cmd[i + 1] == '>')
-		{
-			rdr.nb_redir_app++;
-			if (type == 3 && rdr.nb_redir_app == nb)
-				return (find_lenght_file(cmd + i + 2));
-			i++;
-		}
-		else if (cmd[i] == '<')
-		{
-			rdr.nro++;
-			if (type == 2 && rdr.nro == nb)
-				return (find_lenght_file(cmd + i + 1));
-		}
-		else if (cmd[i] == '>')
-		{
-			rdr.nri++;
-			if (type == 1 && rdr.nri == nb)
-				return (find_lenght_file(cmd + i + 1));
-		}
+		if ((cmd[i] == '<' && cmd[i + 1] == '<') && ++i > -1
+			&& ++rdr.nb_redir_hdoc == nb && type == 4)
+			return (find_lenght_file(cmd + i + 2));
+		else if ((cmd[i] == '>' && cmd[i + 1] == '>') && ++i > -1
+			&& ++rdr.nb_redir_app == nb && type == 3)
+			return (find_lenght_file(cmd + i + 2));
+		else if (cmd[i] == '<' && ++rdr.nro == nb && type == 2)
+			return (find_lenght_file(cmd + i + 1));
+		else if (cmd[i] == '>' && ++rdr.nri == nb && type == 1)
+			return (find_lenght_file(cmd + i + 1));
 		i++;
 	}
 	return (0);
 }
 
+t_len_file	skip_qu_len(char *cmd, t_len_file fi)
+{
+	if (cmd[fi.i] == '\"')
+	{
+		fi.i++;
+		while (cmd[fi.i] != '\"' && cmd[fi.i])
+		{
+			fi.size++;
+			fi.i++;
+		}
+		if (cmd[fi.i])
+			fi.i++;
+	}
+	else if (cmd[fi.i] == '\'')
+	{
+		fi.i++;
+		while (cmd[fi.i] != '\'' && cmd[fi.i])
+		{
+			fi.size++;
+			fi.i++;
+		}
+		if (cmd[fi.i])
+			fi.i++;
+	}
+	return (fi);
+}
+
 int	find_lenght_file(char *cmd)
 {
-	int	i;
-	int	size;
+	t_len_file	fi;
+	int			j;
 
-	size = 0;
-	i = 0;
-	i = skip_blank(cmd);
-	while (cmd[i])
+	fi.size = 0;
+	fi.i = 0;
+	fi.i = skip_blank(cmd);
+	while (cmd[fi.i])
 	{
-		if (cmd[i] == '\"')
+		j = fi.i;
+		fi = skip_qu_len(cmd, fi);
+		if ((cmd[fi.i] == ' ' || cmd[fi.i] == '\t'
+				|| cmd[fi.i] == '<' || cmd[fi.i] == '>') && fi.i == j)
+			return (fi.size);
+		else if (fi.i == j)
 		{
-			i++;
-			while (cmd[i] != '\"' && cmd[i])
-			{
-				size++;
-				i++;
-			}
-			if (cmd[i])
-				i++;
-		}
-		else if (cmd[i] == '\'')
-		{
-			i++;
-			while (cmd[i] != '\'' && cmd[i])
-			{
-				size++;
-				i++;
-			}
-			if (cmd[i])
-				i++;
-		}
-		else if (cmd[i] == ' ' || cmd[i] == '\t'
-			|| cmd[i] == '<' || cmd[i] == '>')
-		{
-			return (size);
-		}
-		else
-		{
-			size++;
-			i++;
+			fi.size++;
+			fi.i++;
 		}
 	}
-	return (i);
+	return (fi.i);
 }
